edb33f266a14d12b30ee507e0424e79579f2ec05
Wiktor Adasiak stacjo ubuntu 22.04 <wadasiak01@gmail.com>
2025-06-10 14:10:18 +0200

    Add implementation for evaluating Reverse Polish Notation

--- Stack/Evaluate Reverse Polish Notation.py
+++ Stack/Evaluate Reverse Polish Notation.py
@@ -1,3 +1,24 @@
+from typing import List
+
+
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        
+        if not tokens:
+            return 0
+        stack = []
+        for element in tokens:
+            if element[-1].isnumeric():
+                stack.append(int(element))
+            else:
+                b = stack.pop()
+                a = stack.pop()
+                if element == "+":
+                    res = a + b
+                elif element == "-":
+                    res = a - b
+                elif element == "*":
+                    res = a * b
+                elif element == "/":
+                    res = a / b
+                stack.append(int(res))
+        return int(stack[0])
