895edadae47373452476737aa39267abcaebb0d1
Wiktor Adasiak stacjo ubuntu 22.04 <wadasiak01@gmail.com>
2025-06-20 14:36:38 +0200

    Add initial implementation for Linked List Cycle detection with sequence tracking

--- Linked List/141. Linked List Cycle.py
+++ Linked List/141. Linked List Cycle.py
@@ -10,6 +10,51 @@ class ListNode:
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         
+        seq = []
+        # for i in range(5):
+        
+        while head:
+            # if head:
+            print(head.val)
+            seq.append(head.val)
+            if len(seq) - len(set(seq)) > 5:
+                return True
+            head = head.next
         
         
-        pass
+        return False
+    
+    
+class Solution:
+    def hasCycle(self, head: ListNode | None) -> bool:
+        slow = fast = head
+        while fast and fast.next:          # dopóki możemy wykonać 2 kroki
+            slow = slow.next               # 1 krok
+            fast = fast.next.next          # 2 kroki
+            if slow is fast:               # spotkanie ⇒ cykl
+                return True
+        return False   
+    
+
+
+class Solution:
+    def hasCycle(self, head: ListNode | None) -> bool:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow is fast:
+                return True
+        return False
+    
+
+
+class Solution:
+    def hasCycle(self, head: ListNode | None) -> bool:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow is fast:
+                return True
+        return False
