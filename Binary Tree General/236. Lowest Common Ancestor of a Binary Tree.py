c815e1f2d1f2d9ac82fe759cbd2193cdda3db53b
Wiktor Adasiak stacjo ubuntu 22.04 <wadasiak01@gmail.com>
2025-06-25 20:32:21 +0200

    Refactor Lowest Common Ancestor implementation to remove duplicate code and improve clarity

--- Binary Tree General/236. Lowest Common Ancestor of a Binary Tree.py
+++ Binary Tree General/236. Lowest Common Ancestor of a Binary Tree.py
@@ -7,4 +7,47 @@ class TreeNode:
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        
+        def dfs(root: TreeNode):
+            if not root:
+                return
+            elif root is p or root is q:
+                return root
+            left = dfs(root.left)
+            right = dfs(root.right)
+            
+            if left and right:
+                return root
+            
+            return left if left else right
+        return dfs(root)
+    
+ 
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        def dfs(root: TreeNode):
+            if not root:
+                return
+            elif root is p or root is q:
+                return root
+            left = dfs(root.left)
+            right = dfs(root.right)
+            if left and right:
+                return root
+            return left if left else right
+        return dfs(root)
+    
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        def dfs(root: TreeNode):
+            if not root:
+                return
+            if root is p or root is q:
+                return root
+            l = dfs(root.left)
+            r = dfs(root.right)
+            
+            if l and r:
+                return root
+            
+            return l if l else r
