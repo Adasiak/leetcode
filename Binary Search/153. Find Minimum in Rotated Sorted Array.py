34bb4836297f1e8f34bacdf5e63e98b67727477e
Wiktor Adasiak stacjo ubuntu 22.04 <wadasiak01@gmail.com>
2025-06-25 17:40:11 +0200

    Add implementation for finding the minimum in a rotated sorted array and LRU Cache; refactor Longest Consecutive Sequence algorithm for clarity and efficiency

---
+++ Binary Search/153. Find Minimum in Rotated Sorted Array.py
@@ -0,0 +1,8 @@
+from typing import List
+
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        return min(nums)
--- hashmap/128. Longest Consecutive Sequence.py
+++ hashmap/128. Longest Consecutive Sequence.py
@@ -1,6 +1,28 @@
 from typing import List
-
+from collections import deque
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        set_nums = set(nums)
+        # cur = 0
+        maxx = deque()
+        for num in set_nums:
+            curr = num
+            tmp = 1
+            if num - 1 not in set_nums:
+                while curr + 1 in set_nums:
+                    tmp += 1
+                    curr += 1
+                if not maxx:
+                    maxx.append(tmp)
+                else:
+                    tmp2 = maxx.pop()
+                    if tmp2 > tmp:
+                        maxx.append(tmp2)
+                    else:
+                        maxx.append(tmp)    
+        max_len = maxx.pop()
+        return max_len 
         
