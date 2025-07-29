7768b7f29e30ca373ac0a519db42a0850e2d1a63
Wiktor Adasiak stacjo ubuntu 22.04 <wadasiak01@gmail.com>
2025-07-29 14:29:28 +0200

    Usuń nieużywaną funkcję rekurencyjną z algorytmu sprawdzania, czy liczba jest szczęśliwa, oraz uprość logikę pętli.

--- hashmap/202. Happy Number.py
+++ hashmap/202. Happy Number.py
@@ -1,26 +1,12 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        def dfs(m):
-            if m < 10:
-                return False
-            digits = list(str(m))
+        for _ in range(10):
+            if n == 1:
+                return True
+            digits = list(str(n))
             ss = 0
             for digit in digits:
                 ss += (int(digit))**2
-            if ss == 1:
-                return True
-            else:
-                dfs(ss)
-            print(digits)
-        dfs(n)
+            n = ss
         return False
-    
-    
-    
-    
-7
-49
-16 + 81 = 97
-81 + 49 = 130
-1 + 9 = 10
-1 + 0 = 1
+
